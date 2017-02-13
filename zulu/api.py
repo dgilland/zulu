82f66d00c9de344749048ccb57139b91c56aba83
Derrick Gilland <dgilland@gmail.com>
2017-02-13 15:43:28 -0500

    Define api alias wrapper functions.

--- zulu/api.py
+++ zulu/api.py
@@ -6,17 +6,43 @@ from .datetime import Zulu
 from .timedelta import Delta
 from ._compat import deprecated
 
-now = Zulu.now
-parse = Zulu.parse
-range = Zulu.range
-span_range = Zulu.span_range
-
 
 def create(*args, **kargs):
     """Factory function to create a new :class:`.Zulu` datetime using the same
     arguments as the class.
+
+    .. seealso:: See :class:`.Zulu` for class signature details.
     """
     return Zulu(*args, **kargs)
+
+
+def now():
+    """Alias to :meth:`.Zulu.now`."""
+    return Zulu.now()
+
+
+def parse(*args, **kargs):
+    """Alias to :meth:`.Zulu.parse`.
+
+    .. seealso:: See :meth:`.Zulu.parse` for function signature details.
+    """
+    return Zulu.parse(*args, **kargs)
+
+
+def range(*args, **kargs):
+    """Alias to :meth:`.Zulu.range`.
+
+    .. seealso:: See :meth:`.Zulu.range` for function signature details.
+    """
+    return Zulu.range(*args, **kargs)
+
+
+def span_range(*args, **kargs):
+    """Alias to :meth:`.Zulu.span_range`.
+
+    .. seealso:: See :meth:`.Zulu.span_range` for function signature details.
+    """
+    return Zulu.span_range(*args, **kargs)
 
 
 def parse_delta(*args, **kargs):
