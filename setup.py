afeb4477052bd6759cdf12eb924b78a06d194532
Derrick Gilland <dgilland@gmail.com>
2018-11-02 02:53:14 +0000

    Move all setuptools.setup() options in setup.py to setup.cfg.

--- setup.py
+++ setup.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
 
 
-package_dir = 'src'
-packages = find_packages(package_dir)
-
-version = {}
-with open('{}/{}/__version__.py'.format(package_dir, packages[0])) as f:
-    exec(f.read(), version)
-
-
 setup(
-    version=version['__version__'],
-    package_dir={'': package_dir},
-    packages=packages,
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
     include_package_data=True
 )
