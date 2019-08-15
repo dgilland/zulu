cd561489be69b610dc96cfe0e0d1e3a5111f662a
Derrick Gilland <dgilland@gmail.com>
2019-08-14 23:45:30 -0400

    Add edge test cases for Zulu.format("YYYY").

--- tests/test_zulu.py
+++ tests/test_zulu.py
@@ -236,6 +236,8 @@ def test_zulu_parse_format(string, kargs, expected):
         (Zulu(2000, 1, 5, 13, 7, 8, 123456), "a", "PM"),
         (Zulu(2000, 1, 5, 13, 7, 8, 123456), "Z", "+0000"),
         (Zulu(), " ", " "),
+        (Zulu(2019, 12, 29, 0, 0), "YYYY", "2019"),
+        (Zulu(2019, 12, 30, 0, 0), "YYYY", "2020"),
     ],
 )
 def test_zulu_format_pattern(dt, opts, expected):
