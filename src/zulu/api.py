7d9d5c026fe721587fa632b85badd8e680a4a9d5
Derrick Gilland <dgilland@gmail.com>
2021-01-07 17:46:15 -0500

    Replace *args and **kwargs in zulu.api function with actual function signatures.

--- src/zulu/api.py
+++ src/zulu/api.py
@@ -4,13 +4,38 @@ from .delta import Delta
 from .zulu import Zulu
 
 
-def create(*args, **kwargs):
+def create(
+    year=1970, month=1, day=1, hour=0, minute=0, second=0, microsecond=0, tzinfo=None, *, fold=0
+):
     """
     Factory function to create a new :class:`.Zulu` datetime using the same arguments as the class.
 
-    .. seealso:: See :class:`.Zulu` for class signature details.
+    Args:
+        year (int|dict): Date year ``1 <= year <= 9999`` or ``dict`` containing keys corresponding
+            to initialization parameter names.
+        month (int): Date month ``1 <= month <= 12``.
+        day (int): Date day ``1 <= day <= number of days in the given month and year``
+        hour (int, optional): Time hour ``0 <= hour < 24``. Defaults to ``0``.
+        minute (int, optional): Time minute ``0 <= minute < 60``. Defaults to ``0``.
+        second (int, optional): Time second ``0 <= second < 60``. Defaults to ``0``.
+        microsecond (int, optional): Time microsecond ``0 <= microsecond < 1000000``. Defaults to
+            ``0``.
+        tzinfo (None|str|tzinfo, optional): Timezone information as either a ``str`` or ``tzinfo``
+            subclass. If value is a ``str``, it will be converted to a ``dateutil.tz`` timezone. If
+            value is ``None``, the datetime values given are assumed to in UTC. Defaults to
+            ``None``.
     """
-    return Zulu(*args, **kwargs)
+    return Zulu(
+        year=year,
+        month=month,
+        day=day,
+        hour=hour,
+        minute=minute,
+        second=second,
+        microsecond=microsecond,
+        tzinfo=tzinfo,
+        fold=fold,
+    )
 
 
 def now():
@@ -18,37 +43,61 @@ def now():
     return Zulu.now()
 
 
-def parse(*args, **kwargs):
+def parse(obj, formats=None, default_tz=None):
     """
-    Alias to :meth:`.Zulu.parse`.
+    Return :class:`.Zulu` object parsed from `obj`.
 
-    .. seealso:: See :meth:`.Zulu.parse` for function signature details.
+    Args:
+        obj (mixed): Object to parse into a :class:`.Zulu` object.
+        formats (str|list, optional): List of string formats to use when parsing. Defaults to
+            ``["ISO8601", "timestamp"]``.
+        default_tz (None|str|tzinfo, optional): Default timezone to use when parsed datetime
+            object does not contain a timezone. Defaults to ``UTC``.
+
+    Returns:
+        :class:`.Zulu`
     """
-    return Zulu.parse(*args, **kwargs)
+    return Zulu.parse(obj, formats=formats, default_tz=default_tz)
 
 
-def range(*args, **kwargs):
+def range(frame, start, end):
     """
-    Alias to :meth:`.Zulu.range`.
+    Yield ranges of :class:`.Zulu` instances from `start` to `end` in steps of time frame, `frame`.
 
-    .. seealso:: See :meth:`.Zulu.range` for function signature details.
+    Args:
+        frame (str): A time frame (e.g. year, month, day, minute, etc).
+        start (datetime): The starting datetime.
+        end (datetime): The ending datetime.
+
+    Yields:
+        :class:`.Zulu`: Datetime values ranging from the given start and end datetimes.
     """
-    return Zulu.range(*args, **kwargs)
+    yield from Zulu.range(frame, start, end)
 
 
-def span_range(*args, **kwargs):
+def span_range(frame, start, end):
     """
-    Alias to :meth:`.Zulu.span_range`.
+    Return a range of time spans from `start` to `end` in steps of time frame, `frame`.
 
-    .. seealso:: See :meth:`.Zulu.span_range` for function signature details.
+    Args:
+        frame (str): A time frame (e.g. year, month, day, minute, etc).
+        start (datetime): The starting datetime.
+        end (datetime): The ending datetime.
+
+    Yields:
+        tuple: 2-element tuple of Zulu time spans
     """
-    return Zulu.span_range(*args, **kwargs)
+    yield from Zulu.span_range(frame, start, end)
 
 
-def parse_delta(*args, **kwargs):
+def parse_delta(obj):
     """
-    Alias to :meth:`.Delta.parse`.
+    Return :class:`.Delta` object parsed from `obj`.
 
-    .. seealso:: See :meth:`.Delta.parse` for function signature details.
+    Args:
+        obj (str|number|timedelta): Object to parse into a :class:`.Delta` object.
+
+    Returns:
+        :class:`.Delta`
     """
-    return Delta.parse(*args, **kwargs)
+    return Delta.parse(obj)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,5 @@
+import inspect
 from unittest import mock
-import warnings
 
 import pytest
 
@@ -10,17 +10,19 @@ parametrize = pytest.mark.parametrize
 
 
 @parametrize(
-    "api_func, target_func",
+    "api_func, target_func, args",
     [
-        (zulu.now, "zulu.zulu.Zulu.now"),
-        (zulu.parse, "zulu.zulu.Zulu.parse"),
-        (zulu.range, "zulu.zulu.Zulu.range"),
-        (zulu.span_range, "zulu.zulu.Zulu.span_range"),
-        (zulu.parse_delta, "zulu.delta.Delta.parse"),
+        (zulu.now, "zulu.Zulu.now", ()),
+        (zulu.parse, "zulu.Zulu.parse", (mock.DEFAULT,)),
+        (zulu.range, "zulu.Zulu.range", (mock.DEFAULT, mock.DEFAULT, mock.DEFAULT)),
+        (zulu.span_range, "zulu.Zulu.span_range", (mock.DEFAULT, mock.DEFAULT, mock.DEFAULT)),
+        (zulu.parse_delta, "zulu.delta.Delta.parse", (mock.DEFAULT,)),
     ],
 )
-def test_api_mapping(api_func, target_func):
+def test_api_mapping(api_func, target_func, args):
     with mock.patch(target_func) as mocked_target_func:
-        api_func()
+        result = api_func(*args)
+        if inspect.isgeneratorfunction(api_func):
+            tuple(result)
 
     assert mocked_target_func.called
